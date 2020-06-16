232dea64ce7450cb93d16757aef71496c38e6573
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-16 15:10:45 -0700

    completed the home page with listing of blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=64)
+  author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
+  body = models.TextField(default = "hey this is a joke!")
+  def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 from django.views.generic import TemplateView, ListView
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
 	template_name = 'home.html'
+	model = Post
